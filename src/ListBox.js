5e59c4be5f658c9cf4c31b4ee5db5fe37bd6cc0c
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2021-03-07 13:14:19 -0800

    title of list displays

--- src/ListBox.js
+++ src/ListBox.js
@@ -1,23 +1,17 @@
 import './ListBox.css';
-import Card from './Card';
 
-const ListBox = () => {
+const ListBox = props => {
   return (
-    <div className='list-container'>
-      <div className='header-container'>
-        <div className='list-name'>
-          Name
-        </div>
-        <input type='button' value='...' className='list-button' />
+    <div className="list-container">
+      <div className="header-container">
+        <div className="list-name">{props.name}</div>
+        <input type="button" value="..." className="list-button" />
       </div>
 
-      <div className='card-container'>
-        <Card />
-        <Card />
-      </div>
-      <input type='button' value='+ Add another card' className='list-button' />
+      <div className="card-container"></div>
+      <input type="button" value="+ Add another card" className="list-button" />
     </div>
-  )
-}
+  );
+};
 
 export default ListBox;
