f9f0cc7d1de52f2715e6a261913c58de52af6444
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2021-03-07 13:48:05 -0800

    add card button functionality

--- src/ListBox.js
+++ src/ListBox.js
@@ -1,17 +1,64 @@
 import './ListBox.css';
+import React, {Component} from 'react';
 
-const ListBox = props => {
-  return (
-    <div className="list-container">
-      <div className="header-container">
-        <div className="list-name">{props.name}</div>
-        <input type="button" value="..." className="list-button" />
+class ListBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      addCard: false,
+      newCardTitle: '',
+    };
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({[e.target.name]: e.target.value});
+  };
+
+  addCard = () => {
+    if (this.state.addCard) {
+      return (
+        <form className="add-list-form" onSubmit={this.handleListSubmit}>
+          <input
+            type="input"
+            name="newCardTitle"
+            value={this.state.newCardTitle}
+            placeholder="Enter a title for this card..."
+            className="card-name-input"
+            onChange={this.handleChange}
+          />
+          <input
+            type="submit"
+            value="Add Card"
+            className="submit-list-button"
+          />
+        </form>
+      );
+    } else {
+      return (
+        <input
+          type="button"
+          value="+ Add another card"
+          className="list-button"
+          onClick={() => this.setState({addCard: true})}
+        />
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="list-container">
+        <div className="header-container">
+          <div className="list-name">{this.props.name}</div>
+          <input type="button" value="..." className="list-button" />
+        </div>
+
+        <div className="card-container"></div>
+        {this.addCard()}
       </div>
-
-      <div className="card-container"></div>
-      <input type="button" value="+ Add another card" className="list-button" />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ListBox;
