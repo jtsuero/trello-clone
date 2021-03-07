e445d453ee6296832473ed03837e122e487ccde0
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2021-03-07 10:51:23 -0800

    add list button and form

--- src/ListBoard.js
+++ src/ListBoard.js
@@ -3,7 +3,13 @@ import ListBox from './ListBox';
 import './ListBoard.css'
 
 class ListBoard extends Component {
-
+  constructor() {
+    super()
+    this.state = {
+      addList: false,
+      newListTitle: '',
+    }
+  }
 
   displayLists = () => {
     return (
@@ -13,10 +19,37 @@ class ListBoard extends Component {
     )
   }
 
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  //onclick show form
+  addItem = () => {
+    if (this.state.addList) {
+      return (
+        <form className='add-list-form'>
+          <input type='input'
+            name='newListTitle'
+            value={this.state.newListTitle}
+            placeholder='Enter list title...'
+            className='list-name-input'
+            onChange={this.handleChange} />
+          <input type='submit' value='Add List' className='submit-list-button' />
+        </form>
+      )
+    } else {
+      return (
+        <input type='button' value='+ Add Another List' className='add-list-button' onClick={() => this.setState({addList: true})} />
+      )
+    }
+  }
+
   render() {
     return (
       <div className='board-container'>
         {this.displayLists()}
+        {this.addItem()}
       </div>
     )
   }
